476ea58e0290cbc6d50accbc3be544412625da2a
ma1ex <matvienko.alexey@gmail.com>
2020-03-22 17:34:51 +0300

    Init project

---
+++ main.py
@@ -0,0 +1,18 @@
+# Snippet Editor Tool for CudaText Editor
+
+# Dependencies: base Python library (tkinter, os)
+
+from tkinter import *
+
+# ------------------------------------------------------------------------------
+
+root = Tk()
+root.title("Add/Edit Snippet - CudaText")
+    
+# Размеры и положение окна: Ширина х Высота + X-pos + Y-pos
+root.geometry('800x450+300+200')
+root.resizable(0, 0)
+
+
+# -------------------------------------------------------------------------/
+root.mainloop()
