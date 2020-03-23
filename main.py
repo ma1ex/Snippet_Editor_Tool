4f2789f1f2271779b1740e3eadc9b483db0feb98
ma1ex <matvienko.alexey@gmail.com>
2020-03-23 17:58:15 +0300

    Added app icon

--- main.py
+++ main.py
@@ -3,16 +3,31 @@
 # Dependencies: base Python library (tkinter, os)
 
 from tkinter import *
+import os
 
 # ------------------------------------------------------------------------------
 
-root = Tk()
-root.title("Add/Edit Snippet - CudaText")
+def main():
+    root = Tk()
+    root.title("Add/Edit Snippet - CudaText")
     
-# Размеры и положение окна: Ширина х Высота + X-pos + Y-pos
-root.geometry('800x450+300+200')
-root.resizable(0, 0)
+    # App icon
+    app_icon = os.path.join(os.path.dirname(__file__), 'res/app_ico.ico')
+    root.iconbitmap(app_icon)
+    
+    # Window dimensions: Width х Height + X-pos + Y-pos
+    w = root.winfo_screenwidth() # current screen width
+    h = root.winfo_screenheight() # current screen height
+    w = w // 2 # screen center
+    h = h // 2 
+    w = w - 300 # offset fron the center
+    h = h - 300
+    root.geometry('800x450+{}+{}'.format(w, h))
+    root.resizable(False, False) # prevent window resizing
 
 
-# -------------------------------------------------------------------------/
-root.mainloop()
+    # -------------------------------------------------------------------------/
+    root.mainloop()
+
+
+if __name__ == "__main__": main()
